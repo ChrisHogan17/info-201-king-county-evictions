384d8a9f354cfc102616c2280676334a1e5fd5ed
unknown <chrishogan17@outlook.com>
2019-03-12 18:41:20 -0700

    Complete map

---
+++ map.R
@@ -0,0 +1,39 @@
+# Load necessary libraries and source necessary files
+
+library(dplyr)
+library(leaflet)
+library(ggmap)
+
+source("analysis.R")
+source("apikeys.R")
+
+# Register api key for geocode() funciton
+
+register_google(key = google_map_key)
+
+# Create lattitude and longitude columns, then fill with coordinates
+evictions_coords <- evictions %>%
+  mutate(city = paste0(city, ", WA")) %>%
+  mutate(long = 0, lat = 0) %>%
+  rename("back_rent" = Amount.of.Back.Rent..n.a.if.none.)
+
+evictions_coords[, c("long", "lat")] <- geocode(evictions_coords$city)
+
+# Create map with new coordinate data
+
+color_map <- colorQuantile("YlOrRd", evictions_coords$back_rent, n = 5)
+
+eviction_map <- leaflet(data = evictions_coords) %>%
+  addTiles() %>%
+  setView(lat = 47.4797, lng = -122.2079, zoom = 9) %>%
+  addCircleMarkers(
+    lat = ~lat,
+    lng = ~long,
+    label = ~(paste0(city, ", Eviction Reason: ", eviction_reason)),
+    radius = ~(tenant_rent / 100),
+    color = ~color_map(back_rent),
+    clusterOptions = markerClusterOptions()
+  ) %>%
+  addLegend(pal = color_map, values = ~back_rent)
+
+

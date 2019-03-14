6c6a22c2727801a65c2b5a4521db32812f9b66e3
unknown <chrishogan17@outlook.com>
2019-03-14 13:19:05 -0700

    Lint file

--- dateplot.R
+++ dateplot.R
@@ -1,24 +1,31 @@
+# Load necessary libraries and source necessary files
+
 library(plotly)
 library(dplyr)
 source("./analysis.R")
 
-# plot function
+# Create plot function
 
 build_date_plot <- function(big_data, city_name) {
-  data <- big_data %>% 
-    filter(city == city_name) %>% 
+
+  data <- big_data %>%
+    filter(city == city_name) %>%
     mutate(month = format(date, "%m"))
-  
+
   month_data <- data.frame(month = "", cases = "", stringsAsFactors = F)
-  
+
+  # Create function for filtering by month and counting the number of cases
+
   get_month_total <- function(data, monthvar) {
-    data <- data %>% 
+    data <- data %>%
       filter(month == monthvar)
     cases <- nrow(data)
     month_data <- rbind(month_data, c(monthvar, cases))
     month_data
   }
-  
+
+  # Gather counts for each month
+
   month_data <- get_month_total(data, "01")
   month_data <- get_month_total(data, "02")
   month_data <- get_month_total(data, "03")
@@ -31,18 +38,18 @@ build_date_plot <- function(big_data, city_name) {
   month_data <- get_month_total(data, "10")
   month_data <- get_month_total(data, "11")
   month_data <- get_month_total(data, "12")
-  
+
   month_data$cases <- as.numeric(month_data$cases)
-  
-  
-  
+
+  # Create a plot of the month counts
+
   date_plot <- plot_ly(
     month_data,
-    x = ~month, 
+    x = ~month,
     y = ~cases,
     type = "scatter",
     mode = "lines+markers"
-  ) %>% 
+  ) %>%
     layout(title = "Monthly Evictions by City",
            xaxis = list(title = "Month"),
            yaxis = list(title = "Number of Cases"))

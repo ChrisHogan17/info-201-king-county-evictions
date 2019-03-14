18b74cfe8dd3591258770c3c69656eec0e5c8e4d
unknown <chrishogan17@outlook.com>
2019-03-14 13:14:47 -0700

    Lint file

--- barchart.R
+++ barchart.R
@@ -6,24 +6,28 @@ source("./analysis.R")
 
 bar_chart <- function(bar_variable) {
   if (bar_variable == "No") {
-    bar_plot <- ggplot(data = all_data, aes(x = city, y = No, the_city = city)) +
+    bar_plot <- ggplot(data = all_data, aes(x = city, y = No,
+                                            the_city = city)) +
       geom_bar(stat = "identity", width = 0.8, fill = "steelblue") +
-      geom_text(aes(label = No), vjust=1.6, color="black",
+      geom_text(aes(label = No), vjust = 1.6, color = "black",
                 position = position_dodge(0.6), size = 3.0) +
-      theme(axis.text.x=element_text(angle=-90,vjust = 0)) +
+      theme(axis.text.x = element_text(angle = -90, vjust = 0)) +
       xlab("City") + ylab("Number of Cases") +
-      ggtitle("The Number of Cases Where Tenant Owes Greater Than One Month Rent")
-    
+      ggtitle(
+        "The Number of Cases Where Tenant Owes Greater Than One Month Rent"
+      )
+   
     ggplotly(bar_plot, tooltip = c("the_city"))
   } else if (bar_variable == "Yes") {
-    bar_plot <- ggplot(data = all_data, aes(x = city, y = Yes, the_city = city)) +
+    bar_plot <- ggplot(data = all_data, aes(x = city, y = Yes,
+                                            the_city = city)) +
       geom_bar(stat = "identity", width = 0.8, fill = "#FF9999") +
-      geom_text(aes(label = Yes), vjust=1.6, color="black",
+      geom_text(aes(label = Yes), vjust = 1.6, color = "black",
                 position = position_dodge(0.6), size = 3.0) +
-      theme(axis.text.x=element_text(angle=-90,vjust = 0)) +
+      theme(axis.text.x = element_text(angle = -90, vjust = 0)) +
       xlab("City") + ylab("Number of Cases") +
       ggtitle("The Number of Cases Where Tenant Owes Less Than One Month Rent")
-    
+
     ggplotly(bar_plot, tooltip = c("the_city"))
   }
 }

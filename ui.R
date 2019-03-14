59a5b6ac5cad2bf5582c09c4e9a03f0dc0e4929b
unknown <chrishogan17@outlook.com>
2019-03-14 13:06:51 -0700

    Lint file

--- ui.R
+++ ui.R
@@ -4,8 +4,8 @@ library(plotly)
 library(leaflet)
 library(shinyWidgets)
 
-source('./analysis.R')
-source('./dateplot.R')
+source("./analysis.R")
+source("./dateplot.R")
 source("./barchart.R")
 
 shinyUI(navbarPage(
@@ -17,66 +17,67 @@ shinyUI(navbarPage(
              includeMarkdown("README.md"))
     )
     ),
-  
+
   # Create a tabPanel to show your scatter plot
   tabPanel(
     "Evictions by Month",
     # Add a titlePanel to your tab
     titlePanel("Evictions by Month Plot"),
-    
+
     setBackgroundColor(
       color = c("#FFC0CB", "#F0E68C"),
       gradient = "linear",
       direction = "left"
     ),
-    
+
     # Create a sidebar layout for this tab (page)
     sidebarLayout(
-      
+
       # Create a sidebarPanel for your controls
       sidebarPanel(
-        
+
         # Make a textInput widget for searching for a state in your scatter plot
-        selectInput("cityvar", "Select a City", 
+        selectInput("cityvar", "Select a City",
                     choices = c(cities),
                     selected = "Seattle")
-        
+
       ),
-      
-      
+
       # Create a main panel, display your plotly Scatter plot
       mainPanel(
         plotlyOutput("by_month_plot")
       )
     )
   ),
-  
+
   tabPanel(
     "Nonpayment of Rent",
     titlePanel("Barchart by Amount of Back Rent"),
-    
+
     sidebarLayout(
-      
+
       sidebarPanel(
-        radioButtons("yes_no", "Did the tenant owe greater than one month's rent?", 
-                     choices = list("Yes", "No"), 
-                     selected = "Yes",
-                     inline = FALSE, width = NULL, choiceNames = NULL,
-                     choiceValues = NULL)
+        radioButtons(
+          "yes_no",
+          "Did the tenant owe greater than one month's rent?",
+          choices = list("Yes", "No"),
+          selected = "Yes",
+          inline = FALSE, width = NULL, choiceNames = NULL,
+          choiceValues = NULL)
       ),
-      
+
       mainPanel(
         plotlyOutput("backrent_chart")
       )
     )
   ),
-  
+
   tabPanel(
     "Evictions Map",
     titlePanel("Evictions Mapped by City"),
-    
+
     sidebarLayout(
-      
+
       sidebarPanel(
         h4("Map will take a moment to load after adjusting date range."),
         p("A mapping of evictions by the city they occured in.
@@ -88,7 +89,7 @@ shinyUI(navbarPage(
           had when evicted (How far behind on rent they were). The size of each
           circle corresponds to the amount of that individual's one month rent
           divided by 100 (for scaling purposes)"),
-        
+
         sliderInput(
           inputId = "date_range",
           label = "Date Range",
@@ -96,9 +97,9 @@ shinyUI(navbarPage(
           max = as.Date("2017-12-31"),
           value = c(as.Date("2017-01-01"), as.Date("2017-12-31"))
         )
-        
+
       ),
-      
+
       mainPanel(
         leafletOutput("map")
       )
